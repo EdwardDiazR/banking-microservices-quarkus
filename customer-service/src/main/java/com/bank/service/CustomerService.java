881cb122e61af7cbf42fcea5203b0f6c3be272bd
EdwardDiazR <edwardadriandiaz2020@gmail.com>
2026-01-09 00:12:28 -0400

    adding methods

--- customer-service/src/main/java/com/bank/service/CustomerService.java
+++ customer-service/src/main/java/com/bank/service/CustomerService.java
@@ -4,4 +4,8 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class CustomerService {
+
+    public void getCustomerById(){}
+    public void createCustomer(){}
+
 }
