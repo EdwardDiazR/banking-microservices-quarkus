6f3bb062bced57d7cf64592b1fec112e31b4a2d4
EdwardDiazR <edwardadriandiaz2020@gmail.com>
2026-01-10 00:12:05 -0400

    add customer class

---
+++ customer-service/src/main/java/com/bank/Customer.java
@@ -0,0 +1,17 @@
+package com.bank;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Customer {
+    private Long id;
+    private String nationalId;
+    private String nationalIdType;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private int age;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastUpdateDate;
+
+}
