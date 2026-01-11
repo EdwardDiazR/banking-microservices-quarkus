b494f0fde14c34b7439c4fb2e6a613c3709a1f44
EdwardDiazR <edwardadriandiaz2020@gmail.com>
2026-01-10 22:08:46 -0400

    updates

--- loan-service/src/main/java/com/bank/loan/controller/LoanResource.java
+++ loan-service/src/main/java/com/bank/loan/controller/LoanResource.java
@@ -1,7 +1,9 @@
-package com.bank.loan;
+package com.bank.loan.controller;
 
 import com.bank.CustomerClient;
+import com.bank.loan.ILoanService;
 import com.bank.loan.dto.LoanDTO;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
@@ -11,7 +13,8 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-@Path("/loan")
+@Path("api/v1/loan")
+@RolesAllowed("")
 public class LoanResource {
 
     @RestClient
--- loan-service/src/main/java/com/bank/loan/service/LoanServiceImpl.java
+++ loan-service/src/main/java/com/bank/loan/service/LoanServiceImpl.java
@@ -7,6 +7,7 @@ import com.bank.loan.dto.LoanDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @ApplicationScoped
 public class LoanServiceImpl implements ILoanService {
@@ -25,5 +26,8 @@ public class LoanServiceImpl implements ILoanService {
         return new LoanDTO("100120", BigDecimal.valueOf(250000));
     }
 
+    public void changePaymentDate(Long loanId,LocalDate newPaymentDate){
+
+    }
 
 }
